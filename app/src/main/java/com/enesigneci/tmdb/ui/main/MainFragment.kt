7e926caa9cdb663e3fd0f5669ab0bbc0ae43295c
Enes İğneci <eigneci@magiclick.com>
2021-05-12 03:34:27 +0300

    Recycler view and text input is added to layout

--- app/src/main/java/com/enesigneci/tmdb/ui/main/MainFragment.kt
+++ app/src/main/java/com/enesigneci/tmdb/ui/main/MainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.enesigneci.tmdb.databinding.MainFragmentBinding
@@ -24,17 +26,20 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.searchLiveData.observe(viewLifecycleOwner, Observer {
-            binding.message.text = it?.results?.get(0)?.originalTitle
-        })
-        viewModel.errorLiveData.observe(viewLifecycleOwner, Observer {
-            binding.message.text = it.message
-        })
         binding.apply {
-            message.setOnClickListener {
-                viewModel.searchInTMDB("Hababam ")
+            tiSearch.editText?.doAfterTextChanged {
+                if(it.toString().length > 2) {
+                    viewModel.searchInTMDB(it.toString())
+                }
             }
         }
+
+        viewModel.searchLiveData.observe(viewLifecycleOwner, Observer {
+            Toast.makeText(context, it.results?.get(0)?.originalTitle, Toast.LENGTH_SHORT).show()
+        })
+        viewModel.errorLiveData.observe(viewLifecycleOwner, Observer {
+            Toast.makeText(context, it.message, Toast.LENGTH_SHORT).show()
+        })
     }
 
 }
